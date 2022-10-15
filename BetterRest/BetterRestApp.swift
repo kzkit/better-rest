eadaa983f3d000df23da54ee2ee2122ff64cc3ee
Zhen Kit Kong <zhenkit1996@gmail.com>
2022-10-16 00:38:51 +0800

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Zhen Kit Kong on 16/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
